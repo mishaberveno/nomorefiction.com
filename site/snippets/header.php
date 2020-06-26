33c28f342e81f25a0010cebf5b964d7c47685f2f
fessl <function44@gmail.com>
2020-06-26 17:14:58 -0400

    changed mberveno.me

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -5,7 +5,7 @@
 	<div class="col-8 header">
 		<div class="header-float-left">
 			<h1><a class="header_link" href="http://storiesbeentold.com">Stories Been Told</a></h1>
-			<h4>by <a href="http://mberveno.me" target="_blank">Misha Berveno</a></h4>
+			<h4>by <a href="http://nocountry.studio/misha-berveno" target="_blank">Misha Berveno</a></h4>
 			<p class="description">
 				Skim through <a href="../stories">stories told</a> and <a href="../books">books read</a>.<br />
 				<a href="https://tinyletter.com/storiesbeentold" target="_blank">Subscribe</a> to receive a personal note when there’s more.
